24bca448d6eba6f94f57afb60f3c19a6591cedd6
rayyush <142962272+rayyush@users.noreply.github.com>
2025-04-30 07:47:53 +0000

    Added the library's functionality

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from difflib import SequenceMatcher
+
+with open ('textfile1.txt') as first_file, open ('textfile2.txt') as second_file:
+    data_file1 = first_file.read()
+    data_file2 = second_file.read()
+    matches = SequenceMatcher(None, data_file1, data_file2).ratio()
+    print(f"The plagiarised content is {matches}%")
